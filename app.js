3ef558fff8418d9181de88519c4d3f54de8edf4f
Monica Williams <monicaestellaw@gmail.com>
2017-05-17 15:51:47 -0700

    got pug templating in project

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
-var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
-var dotenv = require('dotenv');
+const express = require('express'); // Express web server framework
+const app = express();
+const request = require('request'); // "Request" library
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
 dotenv.load();
 
-var client_id = process.env.CLIENT_ID; // Your client id
-var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = process.env.REDIRECT_URI // Your redirect uri
+const client_id = process.env.CLIENT_ID; // Your client id
+const client_secret = process.env.CLIENT_SECRET; // Your secret
+const redirect_uri = process.env.REDIRECT_URI // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const generateRandomString = function(length) {
+  const text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (var i = 0; i < length; i++) {
+  for (const i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
 };
 
-var stateKey = 'spotify_auth_state';
+const stateKey = 'spotify_auth_state';
 
-var app = express();
+app.set('view engine', 'pug')
+
 
 app.use(express.static(__dirname + '/public'))
    .use(cookieParser());
 
-app.get('/', function(req, res) {
-  res.send('hi')
+app.get('/', function(request, response) {
+  response.render('index', {title: 'Hi', message: 'World!'})
 })
 
-// app.get('/login', function(req, res) {
+// app.get('/login', function(request, response) {
 //
-//   var state = generateRandomString(16);
-//   res.cookie(stateKey, state);
+//   const state = generateRandomString(16);
+//   response.cookie(stateKey, state);
 //
 //   // your application requests authorization
-//   var scope = 'user-read-private user-read-email';
-//   res.redirect('https://accounts.spotify.com/authorize?' +
+//   const scope = 'user-read-private user-read-email';
+//   response.redirect('https://accounts.spotify.com/authorize?' +
 //     querystring.stringify({
 //       response_type: 'code',
 //       client_id: client_id,
@@ -57,9 +59,9 @@ app.get('/', function(req, res) {
 //   // your application requests refresh and access tokens
 //   // after checking the state parameter
 //
-//   var code = req.query.code || null;
-//   var state = req.query.state || null;
-//   var storedState = req.cookies ? req.cookies[stateKey] : null;
+//   const code = req.query.code || null;
+//   const state = req.query.state || null;
+//   const storedState = req.cookies ? req.cookies[stateKey] : null;
 //
 //   if (state === null || state !== storedState) {
 //     res.redirect('/#' +
@@ -68,7 +70,7 @@ app.get('/', function(req, res) {
 //       }));
 //   } else {
 //     res.clearCookie(stateKey);
-//     var authOptions = {
+//     const authOptions = {
 //       url: 'https://accounts.spotify.com/api/token',
 //       form: {
 //         code: code,
@@ -84,10 +86,10 @@ app.get('/', function(req, res) {
 //     request.post(authOptions, function(error, response, body) {
 //       if (!error && response.statusCode === 200) {
 //
-//         var access_token = body.access_token,
+//         const access_token = body.access_token,
 //             refresh_token = body.refresh_token;
 //
-//         var options = {
+//         const options = {
 //           url: 'https://api.spotify.com/v1/me',
 //           headers: { 'Authorization': 'Bearer ' + access_token },
 //           json: true
@@ -117,8 +119,8 @@ app.get('/', function(req, res) {
 // app.get('/refresh_token', function(req, res) {
 //
 //   // requesting access token from refresh token
-//   var refresh_token = req.query.refresh_token;
-//   var authOptions = {
+//   const refresh_token = req.query.refresh_token;
+//   const authOptions = {
 //     url: 'https://accounts.spotify.com/api/token',
 //     headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
 //     form: {
@@ -130,7 +132,7 @@ app.get('/', function(req, res) {
 //
 //   request.post(authOptions, function(error, response, body) {
 //     if (!error && response.statusCode === 200) {
-//       var access_token = body.access_token;
+//       const access_token = body.access_token;
 //       res.send({
 //         'access_token': access_token
 //       });
