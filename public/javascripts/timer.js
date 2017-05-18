8b21545fc780e1321703c536da432ad2708eccba
Monica Williams <monicaestellaw@gmail.com>
2017-05-18 00:05:43 -0700

    changes a little styling, starts work on timer class and pseudocodes necessary functionality for timer; view and functionality not yet linked

---
+++ public/javascripts/timer.js
@@ -0,0 +1,23 @@
+class Timer {
+  constructor(  timeLeft ) {
+    this.timeLeft = timeLeft
+  }
+
+  displayTimeLeft() {
+    document.getElementsByClassName('timer-body')[0].innerHTML = "18:00"
+    // timeRemaining = '18:00'
+    //get the element by its id and update the div
+    //first hardcode something in
+    //then allow the user to give input to change the time.
+    //the above comes later
+  }
+
+  //reset timeLeft
+
+  //start timer
+
+  //stop timer
+
+  //pause timer
+
+}
