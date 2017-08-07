215a4e61207957ab1cef755b201a5b27988d9871
Monica Williams <monicaestellaw@gmail.com>
2017-08-07 11:22:05 -0700

    deletes unnecessary comments, sets a default value for our timer to have when the dom is loaded

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -1,29 +1,26 @@
-  // class Timer {
-  // constructor( timeLeft ) {
-  //   this.timeLeft = timeLeft
-  // }
+  class Timer {
+  constructor( timeLeft ) {
+    this.timeLeft = timeLeft
+  }
 
-  // displayTimeLeft() {
-    // document.getElementsByClassName('timer-body')[0].innerHTML = "18:00"
-    // timeRemaining = '18:00'
-    //get the element by its id and update the div
-    //first hardcode something in
-    //then allow the user to give input to change the time.
-    //the above comes later
-  // }
+  displayTimeLeft(num=15) {
+    document.getElementsByClassName('timer-body')[0].innerHTML = num + ":00"
+    // get the element by its id and update the div
+    // first hardcode something in
+    // then allow the user to give input to change the time.
+    // the above comes later
+  }
 
-  //reset timeLeft
+  // reset timeLeft
+  //
+  // start timer
+  //
+  // stop timer
+  //
+  // pause timer
 
-  //start timer
+}
 
-  //stop timer
-
-  //pause timer
-
-// }
-
-// const timer = new Timer( 18 )
-
-// document.addEventListener("DOMContentLoaded", function(event) {
-//   new Timer(18).displayTimeLeft;
-// });
+document.addEventListener("DOMContentLoaded", function(event) {
+  new Timer().displayTimeLeft();
+});
