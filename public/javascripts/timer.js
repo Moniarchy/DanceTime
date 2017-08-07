4427e0fab1f188eed7f0c1bc3fdd96caf0b72f14
Monica Williams <monicaestellaw@gmail.com>
2017-08-07 10:44:28 -0700

    finally got the javascript connected to the pug file. also did work with styles

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -1,16 +1,16 @@
-class Timer {
-  constructor(  timeLeft ) {
-    this.timeLeft = timeLeft
-  }
+  // class Timer {
+  // constructor( timeLeft ) {
+  //   this.timeLeft = timeLeft
+  // }
 
-  displayTimeLeft() {
-    document.getElementsByClassName('timer-body')[0].innerHTML = "18:00"
+  // displayTimeLeft() {
+    // document.getElementsByClassName('timer-body')[0].innerHTML = "18:00"
     // timeRemaining = '18:00'
     //get the element by its id and update the div
     //first hardcode something in
     //then allow the user to give input to change the time.
     //the above comes later
-  }
+  // }
 
   //reset timeLeft
 
@@ -20,4 +20,10 @@ class Timer {
 
   //pause timer
 
-}
+// }
+
+// const timer = new Timer( 18 )
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   new Timer(18).displayTimeLeft;
+// });
